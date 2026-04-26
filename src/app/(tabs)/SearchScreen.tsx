57a6ead68161aec4ff6894207dcb22bd5d02cae6
Eclipse-02 <rafaumar22@gmail.com>
2026-04-26 11:57:09 +0700

    feat: added clear search and scroll to top function in SearchScreen

--- src/app/(tabs)/SearchScreen.tsx
+++ src/app/(tabs)/SearchScreen.tsx
@@ -6,6 +6,8 @@ import { useSearchBooks } from '@/hooks/useSearchBooks';
 import { useSearchQuery } from '@/hooks/useSearchQuery';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { ScrollView, TextInput, TouchableOpacity, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { MotiView } from 'moti';
 
 export default function SearchScreen() {
   const {
@@ -24,8 +26,10 @@ export default function SearchScreen() {
   } = useSearchBooks(debouncedQuery, isSearching);
 
   const books = data?.pages.flatMap((page) => page.books) ?? [];
-
   const total = data?.pages?.[0]?.total ?? 0;
+  const inputRef = useRef<TextInput>(null);
+  const scrollRef = useRef<ScrollView>(null);
+  const [showScrollTop, setShowScrollTop] = useState(false);
 
   const {
     data: subjects,
@@ -39,7 +43,15 @@ export default function SearchScreen() {
       <Header />
 
       {/* Content */}
-      <ScrollView className="flex-1 px-5 pt-4">
+      <ScrollView
+        ref={scrollRef}
+        onScroll={(e) => {
+          const offsetY = e.nativeEvent.contentOffset.y;
+          setShowScrollTop(offsetY > 300);
+        }}
+        scrollEventThrottle={16}
+        className="flex-1 px-5 pt-4"
+      >
 
         {/* Search Input */}
         <View className="mb-4">
@@ -49,13 +61,18 @@ export default function SearchScreen() {
             <TextInput
               value={query}
               onChangeText={setQuery}
+              ref={inputRef}
               placeholder="Search by title or author"
               className="border-b border-gray-800 pl-8 pb-2 text-lg"
             />
 
             {query.length > 0 && (
               <TouchableOpacity
-                onPress={() => setQuery('')}
+                onPress={() => {
+                  setQuery('');
+                  inputRef.current?.focus();
+                  scrollRef.current?.scrollTo({ y: 0, animated: false });
+                }}
                 className="absolute right-0 top-3"
               >
                 <Ionicons name="close-circle" size={20} color="gray" />
@@ -83,6 +100,22 @@ export default function SearchScreen() {
         )}
 
       </ScrollView>
+
+      {showScrollTop && (
+        <MotiView
+          from={{ opacity: 0, translateY: 20 }}
+          animate={{ opacity: 1, translateY: 0 }}
+        >
+          <TouchableOpacity
+            onPress={() =>
+              scrollRef.current?.scrollTo({ y: 0, animated: true })
+            }
+            className="absolute bottom-6 right-6 bg-black p-3 rounded-full shadow-lg"
+          >
+            <Ionicons name="arrow-up" size={20} color="white" />
+          </TouchableOpacity>
+        </MotiView>
+      )}
 
     </View>
   );
